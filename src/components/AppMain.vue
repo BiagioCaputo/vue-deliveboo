96d464864a8e5ac555db9d0d770fed031b6979ed
EricaMarzi <erica.marzi.1993@gmail.com>
2024-04-19 11:40:52 +0200

    row cols section top restaurant (hardcoded)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,7 @@ export default {
             <div class="jumbo-content container">
                 <div class="box-img"></div>
                 <div class="text-center">
-                    <h1 class="jumbo-title">Consegna di cibo a domicilio</h1>
+                    <h1 class="jumbo-title">Consegna di cibo a domicilio e molto altro</h1>
                     <p class="jumbo-subtitle">Ordina dai tuoi ristoranti preferiti, in pochi minuti a casa tua.</p>
                     <div class="input-group mb-3">
                         <span class="input-group-text" type="text">@</span>
@@ -21,16 +21,53 @@ export default {
             </div>
         </div>
         <img src="/img/jumbotron-wave-desktop.svg" alt="footer-wave">
-
-
         <!---------------------------->
-    <h1 class="text-center my-5">DeliveBoo <i class="fa-solid fa-utensils"></i></h1>
+
+        <section class="top-restaurant">
+            <h1 class="text-center my-5">
+                I migliori ristoranti della tua città
+                <i class="fa-solid fa-utensils"></i>
+            </h1>
+
+            <!--img e label dinamici-->
+            <div class="text-center container-rest">
+                <div class="row flex-container">
+                    <div class="col flex-item">
+                        <div class="round-img"></div>
+                    </div>
+                    <div class="col flex-item">
+                        <div class="round-img"></div>
+                    </div>
+                    <div class="col flex-item">
+                        <div class="round-img"></div>
+                    </div>
+                    <div class="col flex-item">
+                        <div class="round-img"></div>
+                    </div>
+                    <div class="col flex-item">
+                        <div class="round-img"></div>
+                    </div>
+                    <div class="col flex-item">
+                        <div class="round-img"></div>
+                    </div>
+                    <div class="col flex-item">
+                        <div class="round-img"></div>
+                    </div>
+                    <div class="col flex-item">
+                        <div class="round-img"></div>
+                    </div>
+                    
+                </div>
+
+            </div>
+
+        </section>
   </main>
 </template>
 
 <style lang="scss" scoped>
 main {
-  height: 800px; /* costruzione */
+  height: fit-content;
 
     /*Wave img */
     img{
@@ -59,12 +96,16 @@ main {
         }
 
         .jumbo-title{
+            width: 70%;
+            margin: 0 auto;
             font-size: 3.5rem;
             font-weight: 700;
         }
     }
 
   }
+
+  /* Input mail */
   .input-group{
     width: 80%;
     margin: 0 auto;
@@ -77,5 +118,40 @@ main {
     }
   }
   
+
+  /* Section Top Restaurant */
+  .top-restaurant{
+    margin-bottom: 50px;
+    h1{
+        font-weight: 700;
+    }
+
+    .container-rest{
+        width: 70%;
+        margin: 0 auto;
+    }
+
+    .row.flex-container{
+        display: flex;
+        margin: 0 -2rem;
+        flex-wrap: wrap;
+
+        .col.flex-item{
+            margin: 2rem;
+            flex-basis: calc(100% / 4 - 4rem);
+
+            display: flex;
+            justify-content: center;
+        }
+    }
+
+    .round-img{
+        width: 150px;
+        height: 150px;
+        background-color: #FFC244;
+        border-radius: 50% 60% 50% 80%;
+    }
 }
+}
+
 </style>
