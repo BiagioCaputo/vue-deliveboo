0e80c70acc04a32e44e37d352eaafa2eab72b7b2
EricaMarzi <erica.marzi.1993@gmail.com>
2024-04-26 12:12:02 +0200

    Sidebar

--- src/assets/pages/ListRestaurantsPage.vue
+++ src/assets/pages/ListRestaurantsPage.vue
@@ -10,8 +10,48 @@ export default {
 
 <template>
     <main class="container-main">
+        <section class="sidebar">
+            <div class="popular-filters">
+                <h6>Filtri popolari</h6>
+                <ul>
+                    <li>
+                        <div class="category-img"><img src="" alt=""></div>
+                        <span>Filtro</span>
+                    </li>
+                    <li>
+                        <div class="category-img"><img src="" alt=""></div>
+                        <span>Filtro</span>
+                    </li>
+                    <li>
+                        <div class="category-img"><img src="" alt=""></div>
+                        <span>Filtro</span>
+                    </li>
 
-        <h1>ristoranti</h1>
+                </ul>
+            </div>
+            <div class="more-filters">
+                <h6>Altri filtri</h6>
+                <ul>
+                    <li>
+                        <div class="category-img"><img src="" alt=""></div>
+                        <span>Filtro</span>
+                    </li>
+                    <li>
+                        <div class="category-img"><img src="" alt=""></div>
+                        <span>Filtro</span>
+                    </li>
+                    <li>
+                        <div class="category-img"><img src="" alt=""></div>
+                        <span>Filtro</span>
+                    </li>
+                    <li>
+                        <div class="category-img"><img src="" alt=""></div>
+                        <span>Filtro</span>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="list"></section>
     </main>
 </template>
 
@@ -20,8 +60,53 @@ export default {
     width: 1100px;
     margin: 0 auto;
 
+    display: flex;
+
     padding-top: 200px;
     padding-bottom: 200px;
     /* da togliere*/
+
+    h6 {
+        font-weight: 700;
+        font-size: 12px;
+    }
+
+    ul {
+        list-style-type: none;
+        padding-left: 0;
+    }
+
+    .sidebar {
+        width: 20%;
+
+        ul {
+            margin-bottom: 20px;
+
+            li {
+                display: flex;
+                align-items: center;
+                gap: 10px;
+                margin-bottom: 10px;
+                border-bottom: 1px solid #e4e4e4;
+                padding: 10px 0;
+
+                .category-img {
+                    width: 35px;
+                    height: 35px;
+                    background-color: #fff3da;
+                    border-radius: 50%;
+                }
+            }
+        }
+
+    }
+
+    .list {
+        /*di costruzione spazi */
+        width: 80%;
+        height: 700px;
+        background-color: rgb(139, 95, 160);
+
+    }
 }
 </style>
