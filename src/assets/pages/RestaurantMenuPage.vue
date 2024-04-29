0eee875e997d221a47fc66f310fb97c93d90c728
LukePadawalker <padurariu.luca@gmail.com>
2024-04-29 11:34:25 +0200

    started css improvement

--- src/assets/pages/RestaurantMenuPage.vue
+++ src/assets/pages/RestaurantMenuPage.vue
@@ -5,23 +5,24 @@ export default {
 </script>
 
 <template>
+    <img src="" alt="restaurant-background-image">
     <div class="container-md">
         <div class="row">
-            <div class="col-lg-9 red">
+            <div class="col-lg-9 red m-4">
                 <h1>restaurant-menu section</h1>
                 <h3>restaurant logo</h3>
                 <div class="row">
                     <div class="col-3 yellow sections-menu">
                         <h1>Sections menu</h1>
                     </div>
-                    <div class="col green">
+                    <div class="col green dishes-section">
                         <h1>Dishes section</h1>
                     </div>
 
                 </div>
             </div>
-            <div class="col blue">
-                <h1>Order window</h1>
+            <div class="col blue order-card m-4">
+                <h1>Order card</h1>
             </div>
         </div>
     </div>
@@ -35,30 +36,42 @@ export default {
 
     .row {
         height: 1000px;
+
+        .red {
+            background-color: red;
+        }
+
+        .blue {
+            background-color: #8a8afb;
+        }
+
+        .yellow {
+            background-color: yellow;
+        }
+
+        .green {
+            background-color: green;
+        }
+
+        .order-card {
+            border-radius: 10px;
+            height: 100px;
+        }
     }
 
-    .red {
-        background-color: red;
-    }
 
-    .blue {
-        background-color: #8a8afb;
-    }
-
-    .yellow {
-        background-color: yellow;
-    }
-
-    .green {
-        background-color: green;
-    }
 
 }
 
 
 @media screen and (max-width: 576px) {
+    .order-card {
+        display: none;
+    }
+
     .sections-menu {
         display: none;
     }
+
 }
 </style>
