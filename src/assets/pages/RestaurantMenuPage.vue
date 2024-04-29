d08ef3107251508cd963644890aca6397ae6362f
LukePadawalker <padurariu.luca@gmail.com>
2024-04-29 10:28:04 +0200

    griding system done

--- src/assets/pages/RestaurantMenuPage.vue
+++ src/assets/pages/RestaurantMenuPage.vue
@@ -5,12 +5,60 @@ export default {
 </script>
 
 <template>
-    <h1>Menù</h1>
+    <div class="container-md">
+        <div class="row">
+            <div class="col-lg-9 red">
+                <h1>restaurant-menu section</h1>
+                <h3>restaurant logo</h3>
+                <div class="row">
+                    <div class="col-3 yellow sections-menu">
+                        <h1>Sections menu</h1>
+                    </div>
+                    <div class="col green">
+                        <h1>Dishes section</h1>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col blue">
+                <h1>Order window</h1>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-h1 {
-    padding-top: 200px;
-    padding-bottom: 200px; //da togliere
+.container-md {
+    background-color: bisque;
+    margin-top: 120px;
+    height: 1000px;
+
+    .row {
+        height: 1000px;
+    }
+
+    .red {
+        background-color: red;
+    }
+
+    .blue {
+        background-color: #8a8afb;
+    }
+
+    .yellow {
+        background-color: yellow;
+    }
+
+    .green {
+        background-color: green;
+    }
+
+}
+
+
+@media screen and (max-width: 576px) {
+    .sections-menu {
+        display: none;
+    }
 }
 </style>
