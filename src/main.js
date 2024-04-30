db74253dcad1e377bf28d711ba13a0985afa5e9b
giamaf <gianluca.maffucci@hotmail.com>
2024-04-30 18:02:48 +0200

    updated main script

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from './App.vue'
-import { router } from './router'
+import App from './App.vue';
+import { router } from './router';
 
 createApp(App).use(router).mount('#app')
