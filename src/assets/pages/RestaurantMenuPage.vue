0a3ea6d0d91ae8f233627b7589fd41e3096c9a91
LukePadawalker <padurariu.luca@gmail.com>
2024-04-30 10:35:10 +0200

    last commit

--- src/assets/pages/RestaurantMenuPage.vue
+++ src/assets/pages/RestaurantMenuPage.vue
@@ -6,13 +6,27 @@ export default {
 
 <template>
     <img class="restaurant-bg" src="/public/img/burgerking.jpeg" alt="restaurant-background-image">
-    <div class="container-md mt-0">
-        <div class="row m-0">
-            <div class="col-lg-9 red m-4 p-0">
-                <h3 class="restaurant-title rounded-start-3 rounded-bottom-3">restaurant logo</h3>
+    <div class="container-md">
+        <div class="row p-0">
+            <div class="col-xl-9">
+                <div class="restaurant-title rounded-start-3 rounded-bottom-3 p-2">
+                    <div class="logo rounded-2 d-flex justify-content-center align-itmes-center">
+                        <img src="" alt="">
+                        logo
+                    </div>
+                    <div class="restaurant-name">McDonald's</div>
+                    <div class="icons">Icons</div>
+                </div>
                 <div class="row m-0 p-0">
                     <div class="col-3 yellow sections-menu m-2 ms-0 rounded-3">
-                        <h1>Sections menu</h1>
+                        <h1>Sections</h1>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
                     </div>
                     <div class="col green dishes-section m-2 me-0 rounded-3">
                         <h1>Dishes section</h1>
@@ -20,8 +34,10 @@ export default {
 
                 </div>
             </div>
-            <div class="col blue order-card m-4 rounded-3">
-                <h1>Order card</h1>
+            <div class="col blue order-card rounded-3 d-flex flex-column justify-content-evenly align-items-center p-4">
+                <h2>Your order</h2>
+                <img src="/public/img/astronaut-grey-scale.svg" alt="image">
+                <p>You've not added any products yet. When you do, you'll see them here!</p>
             </div>
         </div>
     </div>
@@ -31,47 +47,67 @@ export default {
 .restaurant-bg {
     width: 100%;
     filter: blur(8px);
+    -webkit-filter: blur(8px);
     object-fit: cover;
     max-height: 400px;
+    position: relative;
+    z-index: -1;
+
 
 }
 
 .container-md {
-    border: 1px solid rgb(185, 185, 185);
-    margin-top: 120px;
+    margin-top: -120px;
+    position: relative;
     height: 2000px;
+    z-index: 0;
 
     .row {
         height: 500px;
 
-        .red {
-            // background-color: rgb(252, 222, 222);
-        }
-
-        .blue {
-            background-color: #c1c1ff;
-        }
-
-        .yellow {
-            background-color: rgb(255, 255, 204);
-        }
-
-        .green {
-            background-color: rgb(225, 255, 225);
-        }
-
         .restaurant-title {
             width: 100%;
             height: 200px;
-            background-color: rgb(255, 220, 208);
+            background-color: rgb(255, 255, 255);
             border-top-right-radius: 100px;
             box-shadow: 0 3px 10px rgba(0, 0, 0, .1);
+
+            .logo {
+                width: 50px;
+                height: 50px;
+                background-color: aqua;
+                margin-left: 100px;
+                margin-top: -35px;
+            }
+        }
+
+        .dishes-section {
+            box-shadow: 0 3px 10px rgba(0, 0, 0, .1);
+            background-color: white;
+
+        }
+
+        .sections-menu {
+            box-shadow: 0 3px 10px rgba(0, 0, 0, .1);
+            background-color: white;
+
         }
 
         .order-card {
             border-radius: 10px;
             height: 400px;
+            background-color: white;
             box-shadow: 0 3px 10px rgba(0, 0, 0, .1);
+
+            h2 {
+                font-weight: bold;
+            }
+
+            p {
+                text-align: center;
+                padding-right: 30px;
+                padding-left: 30px;
+            }
 
         }
     }
@@ -82,12 +118,28 @@ export default {
 
 
 @media screen and (max-width: 576px) {
-    .order-card {
-        display: none;
+
+
+    .row {
+        padding: 0;
+
+        .order-card {
+            display: none;
+        }
+
+        .sections-menu {
+            display: none;
+        }
+
+        .col-lg-9 {
+            margin: 0;
+            padding: 0;
+        }
     }
 
-    .sections-menu {
-        display: none;
+    .container-md {
+        margin: 0;
+        padding: 0;
     }
 
 }
