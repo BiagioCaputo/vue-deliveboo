f71a26adb1bb2d5a5341d60a4dc0506f1ac4cd9f
LukePadawalker <padurariu.luca@gmail.com>
2024-05-01 12:31:51 +0200

    added animation on homepage

--- src/assets/pages/HomePage.vue
+++ src/assets/pages/HomePage.vue
@@ -55,8 +55,13 @@ export default {
         <!--Jumbotron-->
         <div class="jumbotron">
             <div class="jumbo-content container-desktop">
-                <div class="box-img">
-                    <img src="/img/jumbo-food.png" alt="Food">
+                <div class="animated-image">
+                    <img class="pane-sopra" src="/img/jumbo-food-pane-sopra.png" alt="immagine che fluttua">
+                    <img class="carne-sopra" src="/img/jumbo-food-carne-sopra.png" alt="immagine che fluttua">
+                    <img class="carne-sotto" src="/img/jumbo-food-carne-sotto.png" alt="immagine che fluttua">
+                    <img class="pane-sotto" src="/img/jumbo-food-pane-sotto.png" alt="immagine che fluttua">
+                    <img class="cetriolo" src="/img/jumbo-food-cetriolo.png" alt="immagine che fluttua">
+                    <img class="pomodoro" src="/img/jumbo-food-pomodoro.png" alt="immagine che fluttua">
                 </div>
                 <div class="text-center">
                     <h1 class="jumbo-title">Consegna di cibo a domicilio e molto altro</h1>
@@ -196,8 +201,161 @@ main {
             gap: 30px;
 
 
-            .box-img {
-                width: 450px;
+            .animated-image {
+                position: relative;
+                width: 500px;
+                height: 400px;
+                ;
+
+
+                .pane-sopra {
+                    position: absolute;
+                    animation-name: loop-panesopra;
+                    animation-duration: 3s;
+                    animation-iteration-count: infinite;
+                    animation-timing-function: ease;
+                }
+
+                .carne-sopra {
+                    position: absolute;
+                    animation-name: loop-carnesopra;
+                    animation-duration: 3s;
+                    animation-iteration-count: infinite;
+                    animation-timing-function: ease;
+                    z-index: 2;
+                }
+
+                .carne-sotto {
+                    position: absolute;
+                    animation-name: loop-carnesotto;
+                    animation-duration: 3s;
+                    animation-iteration-count: infinite;
+                    animation-timing-function: ease;
+                    z-index: 1;
+
+                }
+
+
+                .pane-sotto {
+                    position: absolute;
+                    animation-name: loop-panesotto;
+                    animation-duration: 3s;
+                    animation-iteration-count: infinite;
+                    animation-timing-function: ease;
+                }
+
+                .cetriolo {
+                    position: absolute;
+                    animation-name: cetriolo;
+                    animation-duration: 3s;
+                    animation-iteration-count: infinite;
+                    animation-timing-function: ease;
+                }
+
+                .pomodoro {
+                    position: absolute;
+                    animation-name: pomodoro;
+                    animation-duration: 3s;
+                    animation-iteration-count: infinite;
+                    animation-timing-function: ease;
+                }
+
+                @keyframes loop-panesopra {
+                    0% {
+                        transform: translateY(0px);
+                    }
+
+                    50% {
+                        transform: translate(-3px, 25px);
+
+                    }
+
+                    100% {
+                        transform: translateY(0px);
+
+                    }
+                }
+
+                @keyframes loop-carnesopra {
+                    0% {
+                        transform: translateY(0px);
+                    }
+
+                    50% {
+                        transform: translate(-1px, 19px);
+
+                    }
+
+                    100% {
+                        transform: translateY(0px);
+
+                    }
+                }
+
+                @keyframes loop-carnesotto {
+                    0% {
+                        transform: translateY(0px);
+                    }
+
+                    50% {
+                        transform: translateY(10px);
+
+                    }
+
+                    100% {
+                        transform: translateY(0px);
+
+                    }
+                }
+
+                @keyframes loop-panesotto {
+                    0% {
+                        transform: translateY(0px);
+                    }
+
+                    50% {
+                        transform: translateY(7px);
+
+                    }
+
+                    100% {
+                        transform: translateY(0px);
+
+                    }
+                }
+
+                @keyframes cetriolo {
+                    0% {
+                        transform: translateY(0px);
+                    }
+
+                    50% {
+                        transform: translate(-12px, 15px);
+
+                    }
+
+                    100% {
+                        transform: translateY(0px);
+
+                    }
+                }
+
+                @keyframes pomodoro {
+                    0% {
+                        transform: translateY(0px);
+                    }
+
+                    50% {
+                        transform: translate(15px, -7px);
+
+                    }
+
+                    100% {
+                        transform: translateY(0px);
+
+                    }
+                }
+
             }
 
             .jumbo-title {
