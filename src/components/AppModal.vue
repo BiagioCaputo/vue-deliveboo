68dbb51628c11790de4de41c96d6eae9e8fed38f
giamaf <gianluca.maffucci@hotmail.com>
2024-05-02 12:47:22 +0200

    make modal component

---
+++ src/components/AppModal.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+    // Javascript Logic
+    name: 'Modal',
+    props: {
+        isActive: Boolean,
+        title: String,
+        message: String
+    },
+    emits: ['erase-cart-close-modal', 'close-modal']
+};
+</script>
+
+<template>
+    <div class="modal" :class="{ 'is-active': isActive }">
+        <div class="modal-background"></div>
+        <div class="modal-card my-modal-bg glass ">
+            <header class="modal-card-head d-flex  justify-content-center">
+                <p class="modal-card-title fw-bolder fs-3">{{ title }}</p>
+            </header>
+            <section class="modal-card-body fs-5">
+                <p>{{ message }}</p>
+            </section>
+            <footer class="modal-card-foot bg-transparent d-flex justify-content-center">
+                <button class="custom-primary-btn btn is-success me-3"
+                    @click="$emit('erase-cart-close-modal', !isActive)">Sì</button>
+                <button class="custom-primary-btn btn button-refuse is-success"
+                    @click="$emit('close-modal', !isActive)">No</button>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.modal {
+    background-color: rgba(0, 0, 0, 0.526);
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    justify-content: center;
+    align-items: center;
+    z-index: 10;
+
+    .custom-primary-btn:hover {
+        color: white;
+    }
+
+    .button-refuse {
+        background-color: #e9f8f5;
+        color: #00A082;
+
+    }
+
+    .button-refuse:hover {
+        background-color: #00A082;
+        color: white;
+    }
+}
+
+.modal.is-active {
+    display: flex;
+}
+
+.my-modal-bg {
+    background-color: white;
+    padding: 20px;
+    border-radius: 15px;
+    color: black;
+}
+</style>
